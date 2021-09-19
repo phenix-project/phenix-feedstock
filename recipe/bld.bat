40878c65e3b9af42e2790bed46ba4f24f0632614
Billy K. Poon <bkpoon@lbl.gov>
2021-09-19 12:45:30 -0700

    Clean up build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -106,4 +106,5 @@ attrib -H %LIBRARY_BIN%\libtbx.show_dist_paths.bat
 
 REM clean up and wait 10 minutes
 call %CONDA%\condabin\conda.bat clean -y --all
+call %CONDA%\condabin\conda.bat build purge -y
 timeout /T 600
