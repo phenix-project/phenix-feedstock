15ec55f40b8f1db930e51b24443e01fe44cd204a
Billy K. Poon <bkpoon@lbl.gov>
2022-09-05 15:07:47 -0700

    Fix more PyQuante errors

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -49,6 +49,7 @@ call futurize -f lib2to3.fixes.fix_tuple_params -wn .\modules\PyQuante
 
 copy %RECIPE_DIR%\process_saccharides.py .\modules\elbow\elbow\scripts\process_saccharides.py
 copy %RECIPE_DIR%\process_amino_acid_parentage_from_chemical_components.py .\modules\elbow\elbow\scripts\process_amino_acid_parentage_from_chemical_components.py
+copy %RECIPE_DIR%\ThomasFermi.py .\modules\PyQuante\PyQuante\ThomasFermi.py
 
 REM copy bootstrap.py
 copy modules\cctbx_project\libtbx\auto_build\bootstrap.py .
