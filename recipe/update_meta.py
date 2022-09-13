51b822c52df5d5154c79464ca7179c18770d33b1
Billy K. Poon <bkpoon@lbl.gov>
2022-09-13 11:28:57 -0700

    Switch to encoded file

--- recipe/update_meta.py
+++ recipe/update_meta.py
@@ -82,10 +82,10 @@ def run():
   if raw_url is not None:
     file_url = urlsplit(raw_url)
     new_url = list(file_url)
-    new_url[3] = 'format=file&subPath=%2Fphenix.tar.gz'
+    new_url[3] = 'format=file&subPath=%2Fphenix.enc'
     unix_url = urlunsplit(new_url)
     # Windows does not like the %2F
-    new_url[3] = 'format=file&subPath=/phenix.tar.gz'
+    new_url[3] = 'format=file&subPath=/phenix.enc'
     win_url = urlunsplit(new_url)
 
   # modify meta.yaml to use new version and URL
