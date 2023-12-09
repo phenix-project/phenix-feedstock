bb9eac10270f1cf0c1d56d4dc01c729cabf5ca20
Billy K. Poon <bkpoon@lbl.gov>
2023-12-09 15:09:46 -0800

    Pause Windows Start menu

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -201,17 +201,17 @@ REM copy dxtbx_flumpy.so separately since it does not end it *_ext.so
 REM copy ./build/lib/dxtbx_flumpy.so ${SP_DIR}/../lib-dynload/
 
 REM copy items for Start Menu
-set MENU_DIR=%PREFIX%\Menu
-mkdir %MENU_DIR%
+@REM set MENU_DIR=%PREFIX%\Menu
+@REM mkdir %MENU_DIR%
 
-copy .\modules\gui_resources\icons\custom\phenix.ico %MENU_DIR%
-if %errorlevel% neq 0 exit /b %errorlevel%
+@REM copy .\modules\gui_resources\icons\custom\phenix.ico %MENU_DIR%
+@REM if %errorlevel% neq 0 exit /b %errorlevel%
 
-%PYTHON% %RECIPE_DIR%\scripts\win_update_menu.py --file %RECIPE_DIR%\menu-windows.json --version %PKG_VERSION%
-if %errorlevel% neq 0 exit /b %errorlevel%
+@REM %PYTHON% %RECIPE_DIR%\scripts\win_update_menu.py --file %RECIPE_DIR%\menu-windows.json --version %PKG_VERSION%
+@REM if %errorlevel% neq 0 exit /b %errorlevel%
 
-copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\phenix.json
-if %errorlevel% neq 0 exit /b %errorlevel%
+@REM copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\phenix.json
+@REM if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM clean up cbflib
 move %SP_DIR%\cbflib\pycbf\pycbf.py %SP_DIR%
